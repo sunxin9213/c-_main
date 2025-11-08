4f4ed674b2a9f9c7401eb2572396973776ec56e2
sunxin <13591012592@163.com>
2025-11-08 05:55:25 +0000

    类的五大原则和零原则

--- include/common.h
+++ include/common.h
@@ -2,5 +2,5 @@
 
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 void print_str(const std::string& string_in);
--- include/test.h
+++ include/test.h
@@ -44,3 +44,64 @@ class TEST {
 
 };
 
+// 五大原则（Rule of Five）
+// 当一个类需要手动管理资源时，应该正确定义以下五个特殊成员函数：
+// 1. 析构函数（Destructor）
+// 2. 拷贝构造函数（Copy Constructor）
+// 3. 拷贝赋值运算符（Copy Assignment Operator）
+// 4. 移动构造函数（Move Constructor）
+// 5. 移动赋值运算符（Move Assignment Operator）
+//五大原则，需要手动管理资源的情况，可参考如下类
+
+//零原则：也就是尽量使用智能指针和标准库去管理资源，而不是使用五大原则，这样代码更简洁且无异常
+class RuleOfFive {
+private:
+    int* data;
+    size_t size;
+    
+public:
+    // 1. 构造函数
+    RuleOfFive(size_t s = 0) : size(s), data(size ? new int[size] : nullptr) {}
+    
+    // 2. 析构函数
+    ~RuleOfFive() {
+        delete[] data;
+    }
+    
+    // 3. 拷贝构造函数
+    RuleOfFive(const RuleOfFive& other) 
+        : size(other.size), data(size ? new int[size] : nullptr) {
+        std::copy(other.data, other.data + size, data);
+    }
+    
+    // 4. 拷贝赋值运算符
+    RuleOfFive& operator=(const RuleOfFive& other) {
+        if (this != &other) {
+            delete[] data;
+            size = other.size;
+            data = size ? new int[size] : nullptr;
+            std::copy(other.data, other.data + size, data);
+        }
+        return *this;
+    }
+    
+    // 5. 移动构造函数
+    RuleOfFive(RuleOfFive&& other) noexcept 
+        : data(other.data), size(other.size) {
+        other.data = nullptr;
+        other.size = 0;
+    }
+    
+    // 6. 移动赋值运算符
+    RuleOfFive& operator=(RuleOfFive&& other) noexcept {
+        if (this != &other) {
+            delete[] data;
+            data = other.data;
+            size = other.size;
+            other.data = nullptr;
+            other.size = 0;
+        }
+        return *this;
+    }
+};
+
